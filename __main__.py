43372f584cb9857ddf7e68baef22216f7c1fb68a
adam.marples <adam.marples@sainsburys.co.uk>
2020-04-01 11:01:23 +0100

    black

--- __main__.py
+++ __main__.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
-from pprint import pprint 
-import datetime 
+from pprint import pprint
+import datetime
 import os
 
 import spotipy
@@ -11,41 +11,41 @@ import dotenv
 from scrape import list_feeds
 
 
-dotenv.load_dotenv('.spotify', override=True)
+dotenv.load_dotenv(".spotify", override=True)
 
 
 def make_token():
-	return spotipy.util.prompt_for_user_token(
-		'adammarples',
-		'playlist-modify-public',
-	)
+    return spotipy.util.prompt_for_user_token("adammarples", "playlist-modify-public",)
 
 
 @dataclass
 class Spotify:
-	username = 'adammarples'
-	client = spotipy.Spotify(auth=make_token())
+    username = "adammarples"
+    client = spotipy.Spotify(auth=make_token())
 
 
 def search_for_uri(track):
-	try:
-		results = Spotify.client.search(q=f'{track.artist} {track.name}')
-		items = list(results.values())[0]['items']
-	except spotipy.client.SpotifyException:
-		return
-	if not items:
-		return
-	item = items.pop()
-	print(track)
-	return item['uri']
+    try:
+        results = Spotify.client.search(q=f"{track.artist} {track.name}")
+        items = list(results.values())[0]["items"]
+    except spotipy.client.SpotifyException:
+        return
+    if not items:
+        return
+    item = items.pop()
+    print(track)
+    return item["uri"]
+
 
 if __name__ == "__main__":
-	playlists = Spotify.client.user_playlists(Spotify.username)['items']
-	playlist_titles = [playlist['name'] for playlist in playlists]
-	for feed in list_feeds():
-		print(f'{feed.title}')
-		if feed.title not in playlist_titles:
-			playlist = Spotify.client.user_playlist_create(Spotify.username, feed.title)
-			uris = [search_for_uri(track) for track in feed.tracks]
-			uris = [uri for uri in uris if uri is not None]
-			Spotify.client.user_playlist_add_tracks(Spotify.username, playlist['id'], uris, position=None)
+    playlists = Spotify.client.user_playlists(Spotify.username)["items"]
+    playlist_titles = [playlist["name"] for playlist in playlists]
+    for feed in list_feeds():
+        print(f"{feed.title}")
+        if feed.title not in playlist_titles:
+            playlist = Spotify.client.user_playlist_create(Spotify.username, feed.title)
+            uris = [search_for_uri(track) for track in feed.tracks]
+            uris = [uri for uri in uris if uri is not None]
+            Spotify.client.user_playlist_add_tracks(
+                Spotify.username, playlist["id"], uris, position=None
+            )
--- scrape.py
+++ scrape.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 import unidecode
 import feedparser
-import pandas as pd 
+import pandas as pd
 
 
 @dataclass
@@ -18,24 +18,27 @@ class Feed:
 
     @property
     def tracks(self):
-        _, df, *rest = pd.read_html(self.link, encoding='utf-8')
+        _, df, *rest = pd.read_html(self.link, encoding="utf-8")
         df = df.applymap(decode)
-        df = df[['Artist', 'Track']].dropna()
-        return tuple(Track(artist, name) for artist, name in zip(df['Artist'], df['Track']) if artist != name)
+        df = df[["Artist", "Track"]].dropna()
+        return tuple(
+            Track(artist, name)
+            for artist, name in zip(df["Artist"], df["Track"])
+            if artist != name
+        )
 
 
 def decode(x):
-	try:
-		return unidecode.unidecode(x)
-	except AttributeError:
-		return x
+    try:
+        return unidecode.unidecode(x)
+    except AttributeError:
+        return x
 
 
 def list_feeds():
-    FEED_URL = 'http://www.wfmu.org/playlistfeed/SP.xml'
+    FEED_URL = "http://www.wfmu.org/playlistfeed/SP.xml"
     feed = feedparser.parse(FEED_URL)
-    for entry in feed['entries']:
+    for entry in feed["entries"]:
         yield Feed(
-            title=entry['title'],
-            link=entry['link'],
+            title=entry["title"], link=entry["link"],
         )
